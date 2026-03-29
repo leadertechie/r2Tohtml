a3d43e85c4e4f6ed28ab63e1632189f769cc48f8
leadertechie <leadertechie@gmail.com>
2026-03-29 15:51:01 +0800

    feat: re-export md2html types for convenience (#2)
    Co-authored-by: Milu <milu@techieleader.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './types';
 export * from './cache';
 export * from './frontmatter';
 export * from './loader';
+export type { ContentNode, PipelineConfig } from '@leadertechie/md2html';
