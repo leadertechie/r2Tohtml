aacd4b9db496fd6638975fad24cd277a7cd5dca2
Milu <milu@techieleader.com>
2026-03-29 15:43:48 +0800

    feat: re-export md2html types for convenience

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ export * from './types';
 export * from './cache';
 export * from './frontmatter';
 export * from './loader';
+export type { ContentNode, PipelineConfig } from '@leadertechie/md2html';
 //# sourceMappingURL=index.d.ts.map
