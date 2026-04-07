9741490dff6260b20563d6ebddf8ab26733627e2
leadertechie <leadertechie@gmail.com>
2026-04-07 18:45:50 +0800

    Upgraded md2html version (#8)
    * Upgraded md2html version
    
    ---------
    
    Co-authored-by: Milu <milu@techieleader.com>

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './types';
 export * from './cache';
 export * from './frontmatter';
 export * from './loader';
-export type { ContentNode, PipelineConfig } from '@leadertechie/md2html';
+export type { ContentNode, PipelineConfig, MarkdownPipeline } from '@leadertechie/md2html';
